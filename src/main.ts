e4c387ac8e0300e32c6913b0441108bb9b99f174
BOO-0-K <boo970328@gmail.com>
2024-12-09 17:32:13 +0900

    Settings...

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 5000;
+  await app.listen(port);
+  console.log(`🚀 ${port}번 포트에서 서버 실행 중입니다.`);
 }
 bootstrap();
